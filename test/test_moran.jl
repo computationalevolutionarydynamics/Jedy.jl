a7b471ab8d3bf1523aba818f3d5a6e49d82eda70
Nikolas M Skoufis <n.skoufis@gmail.com>
2014-09-15 23:48:03 +1000

    Added some tests for the Population type and related methods, and added a custom error handler that gives colored output

--- test/test_moran.jl
+++ test/test_moran.jl
@@ -1,21 +1,53 @@
 module TestMoran
-    using Optim
-    using Base.Test
+using Optim
+using Base.Test
+include("../src/Jedy.jl")
 
-    f(X) = (10 - X[1, 1])^2 + (0 - X[1, 2])^2 + (0 - X[2, 1])^2 + (5 - X[2, 2])^2
+# Define a custom error handler to give formatted success or failures
+customHandler(r::Test.Success) = print_with_color(:green, "Success on $(r.expr)\n")
+customHandler(r::Test.Failure) = error("$(r.expr)")
+customHandler(r::Test.Error) = rethrow(r)
 
-    function g!(X, S)
-        S[1, 1] = -20 + 2 * X[1, 1]
-        S[1, 2] = 2 * X[1, 2]
-        S[2, 1] = 2 * X[2, 1]
-        S[2, 2] = -10 + 2 * X[2, 2]
-        return
-    end
+Test.with_handler(customHandler) do
 
-    res = optimize(f, g!, eye(2), method = :gradient_descent)
+f(X) = (10 - X[1, 1])^2 + (0 - X[1, 2])^2 + (0 - X[2, 1])^2 + (5 - X[2, 2])^2
 
-    @test norm(vec(res.minimum - [10.0 0.0; 0.0 5.0])) < 10e-8
+function g!(X, S)
+    S[1, 1] = -20 + 2 * X[1, 1]
+    S[1, 2] = 2 * X[1, 2]
+    S[2, 1] = 2 * X[2, 1]
+    S[2, 2] = -10 + 2 * X[2, 2]
+    return
+end
 
-    # TODO: Get finite differencing to work for generic arrays as well
-    # optimize(f, eye(2), method = :gradient_descent)
+res = optimize(f, g!, eye(2), method = :gradient_descent)
+
+@test norm(vec(res.minimum - [10.0 0.0; 0.0 5.0])) < 10e-8
+
+# TODO: Get finite differencing to work for generic arrays as well
+# optimize(f, eye(2), method = :gradient_descent)
+
+# Test the constructor of the Population type
+# Test that the correct total population is generated
+testArr = [10 for _ = 1:5]
+testPop = Population(testArr)
+@test testPop.totalPop == 50
+
+# Test that an error is thrown if the sum is not correct
+@test_throws ArgumentError Population(testArr, 40)
+
+# Test that an error is thrown if the total population is zero
+@test_throws ArgumentError Population([0, 0, 0])
+
+# Test that an error is thrown if the population has negative values
+@test_throws ArgumentError Population([-1, 2, 0])
+
+# Test fitness function for matrix games
+# Test that the fitness is correct for a simple game and a simple population
+payoffMatrix = [0 1; 1 0]
+pop = Population([10,10])
+@test fitness(pop, payoffMatrix) == [10/19,10/19]
+
+end
+
 end
